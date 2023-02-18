988e0ac7032aae0cabfca1809cc383441ccf9664
madml <manuel.diaz@mercadolibre.com>
2023-02-18 19:58:37 -0300

    create struct config

--- app.go
+++ app.go
@@ -1,8 +1,22 @@
 package truequeslib
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
 type Api struct {
 	Name    string
 	Version string
+	Config  Config
+	Server  *http.Server
+	Router  *gin.Engine
 }
 
 type BaseResponse struct {
@@ -10,11 +24,35 @@ type BaseResponse struct {
 	Error   string `json:"error"`
 }
 
-func InitApi(name string, version string) Api {
+func NewApi(name string, version string) (Api, error) {
+	f, err := os.OpenFile(name+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return Api{}, fmt.Errorf("err-setting_logFile: %w", err)
+	}
+
+	log.SetOutput(f)
+	log.Println("Init app", version)
+
+	config, err := LoadConfig(name + ".yml")
+	if err != nil {
+		return Api{}, fmt.Errorf("err-init_config: %w", err)
+	}
+
+	gin.SetMode(config.GinMode)
+	router := gin.Default()
+	server := &http.Server{
+		Addr:         ":" + strconv.Itoa(config.Server.Port),
+		Handler:      router,
+		ReadTimeout:  time.Duration(config.Server.ReadTimeout) * time.Second,
+		WriteTimeout: time.Duration(config.Server.WriteTimeout) * time.Second,
+	}
+
 	return Api{
 		Name:    name,
 		Version: version,
-	}
+		Router:  router,
+		Server:  server,
+	}, nil
 }
 
 func (api *Api) GetBaseReponse(err string) BaseResponse {
