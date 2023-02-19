bcce52f6a51255118f8afeb4953e587fa669bf3f
MD <manueldiaz1000@gmail.com>
2023-02-18 21:12:30 -0300

    refactor code

--- app.go
+++ app.go
@@ -9,14 +9,23 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v2"
 )
 
-type Api struct {
+type App struct {
 	Name    string
 	Version string
 	Config  Config
-	Server  *http.Server
-	Router  *gin.Engine
+	Server  Server `yaml:"server"`
+}
+
+type Server struct {
+	HttpServer   *http.Server
+	Router       *gin.Engine
+	GinMode      string `yaml:"gin_mode"`
+	Port         int    `yaml:"port"`
+	ReadTimeout  int    `yaml:"read_timeout"`
+	WriteTimeout int    `yaml:"write_timeout"`
 }
 
 type BaseResponse struct {
@@ -24,38 +33,57 @@ type BaseResponse struct {
 	Error   string `json:"error"`
 }
 
-func NewApi(name string, version string) (Api, error) {
+func NewApp(name string, version string) (App, error) {
 	f, err := os.OpenFile(name+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		return Api{}, fmt.Errorf("err-setting_logFile: %w", err)
+		return App{}, fmt.Errorf("err-setting_logFile: %w", err)
 	}
 
 	log.SetOutput(f)
 	log.Println("Init app", version)
 
-	config, err := LoadConfig(name + ".yml")
-	if err != nil {
-		return Api{}, fmt.Errorf("err-init_config: %w", err)
-	}
-
-	gin.SetMode(config.GinMode)
-	router := gin.Default()
-	server := &http.Server{
-		Addr:         ":" + strconv.Itoa(config.Server.Port),
-		Handler:      router,
-		ReadTimeout:  time.Duration(config.Server.ReadTimeout) * time.Second,
-		WriteTimeout: time.Duration(config.Server.WriteTimeout) * time.Second,
-	}
-
-	return Api{
+	app := App{
 		Name:    name,
 		Version: version,
-		Router:  router,
-		Server:  server,
-	}, nil
+	}
+
+	err = app.LoadConfig()
+	if err != nil {
+		return App{}, fmt.Errorf("err-load_config: %w", err)
+	}
+
+	return app, nil
 }
 
-func (api *Api) GetBaseReponse(err string) BaseResponse {
+func (api *App) LoadConfig() error {
+	cfgFilename := api.Name + ".yml"
+	yamlFile, err := os.ReadFile(cfgFilename)
+	if err != nil {
+		return fmt.Errorf("err-reading_yamlFile %s: %w", cfgFilename, err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, &api)
+	if err != nil {
+		return fmt.Errorf("err-unmarshalling_yamlFile %s: %w", cfgFilename, err)
+	}
+
+	gin.SetMode(api.Server.GinMode)
+
+	router := gin.Default()
+	httpServer := &http.Server{
+		Addr:         ":" + strconv.Itoa(api.Server.Port),
+		Handler:      router,
+		ReadTimeout:  time.Duration(api.Server.ReadTimeout) * time.Second,
+		WriteTimeout: time.Duration(api.Server.WriteTimeout) * time.Second,
+	}
+
+	api.Server.Router = router
+	api.Server.HttpServer = httpServer
+
+	return nil
+}
+
+func (api *App) GetBaseReponse(err string) BaseResponse {
 	return BaseResponse{
 		Version: api.Version,
 		Error:   err,
--- config.go
+++ config.go
@@ -1,41 +1,9 @@
 package truequeslib
 
-import (
-	"fmt"
-	"os"
-
-	"gopkg.in/yaml.v2"
-)
-
 type Config struct {
-	GinMode   string    `yaml:"gin_mode"`
-	Server    Server    `yaml:"server"`
 	Endpoints Endpoints `yaml:"endpoints"`
-}
-
-type Server struct {
-	Port         int `yaml:"port"`
-	ReadTimeout  int `yaml:"read_timeout"`
-	WriteTimeout int `yaml:"write_timeout"`
 }
 
 type Endpoints struct {
 	Adverts string `yaml:"adverts"`
-}
-
-func LoadConfig(cfgFileName string) (Config, error) {
-	var config Config
-
-	yamlFile, err := os.ReadFile(cfgFileName)
-	if err != nil {
-		return config, fmt.Errorf("err-reading_yamlFile %s: %w", cfgFileName, err)
-	}
-
-	err = yaml.Unmarshal(yamlFile, &config)
-
-	if err != nil {
-		return config, fmt.Errorf("err-unmarshalling_yamlFile %s: %w", cfgFileName, err)
-	}
-
-	return config, nil
 }
